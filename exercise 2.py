908b9ccdcd5693aa2696d728b8f8c568e781682c
Beratcan Özkan <beratcanozkan@ogr.iu.edu.tr>
2023-01-24 06:25:46 +0300

    Create exercise 2.py

---
+++ exercise 2.py
@@ -0,0 +1,8 @@
+num1 = 6
+num2 = 66
+if num1 > num2:
+    print("number1 is greater than number2")
+elif:
+    print("number2 is greater than number1")
+else:
+    print("number2 equals to number1")
